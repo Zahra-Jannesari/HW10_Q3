70c5f3921ff462cefcb136a69945b4227d3609e7
Zarisa <jannesari.zarisa@gmail.com>
2022-03-13 18:34:45 +0330

    setup the menu

--- app/src/main/java/com/zarisa/hw10_q3/MainActivity.kt
+++ app/src/main/java/com/zarisa/hw10_q3/MainActivity.kt
@@ -2,10 +2,23 @@ package com.zarisa.hw10_q3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import com.zarisa.hw10_q3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
