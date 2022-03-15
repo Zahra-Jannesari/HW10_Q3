60cee620260af31aae0912d13a4f58e133c507e1
Zarisa <jannesari.zarisa@gmail.com>
2022-03-15 11:18:07 +0330

    add data bank information

---
+++ app/src/main/java/com/zarisa/hw10_q3/DataBank.kt
@@ -0,0 +1,16 @@
+package com.zarisa.hw10_q3
+
+object DataBank {
+    var userName:String=""
+    var userNationalID:Int=0
+    var userPhone:Int=0
+    var userAddress:String=""
+    var userBloodType:BloodType=BloodType.Unknown
+}
+enum class BloodType{
+    APlus,AMinus,
+    BPlus,BMinus,
+    ABPlus,ABMinus,
+    OPlus,OMinus,
+    Unknown
+}
