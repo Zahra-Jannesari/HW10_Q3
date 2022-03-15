e3a12c89bb4a32f12034d4a82d4ca5777cc00c2c
Zarisa <jannesari.zarisa@gmail.com>
2022-03-15 11:26:58 +0330

    fix profile view model

--- app/src/main/java/com/zarisa/hw10_q3/ViewModels.kt
+++ app/src/main/java/com/zarisa/hw10_q3/ViewModels.kt
@@ -3,7 +3,36 @@ package com.zarisa.hw10_q3
 import androidx.lifecycle.ViewModel
 
 class ViewModelProfile:ViewModel(){
-
+    fun getUserName():String{
+        return DataBank.userName
+    }
+    fun setUserName(newName:String){
+        DataBank.userName=newName
+    }
+    fun getUserId():Int{
+        return DataBank.userNationalID
+    }
+    fun setUserId(newId:Int){
+        DataBank.userNationalID=newId
+    }
+    fun getUserPhone():Int{
+        return DataBank.userPhone
+    }
+    fun setUserName(newPhone:Int){
+        DataBank.userPhone=newPhone
+    }
+    fun getUserAddress():String{
+        return DataBank.userAddress
+    }
+    fun setUserAddress(newAddress:String){
+        DataBank.userAddress=newAddress
+    }
+    fun getUserBloodType():BloodType{
+        return DataBank.userBloodType
+    }
+    fun setUserName(newBloodType: BloodType){
+        DataBank.userBloodType=newBloodType
+    }
 }
 class ViewModelItems:ViewModel(){
 
