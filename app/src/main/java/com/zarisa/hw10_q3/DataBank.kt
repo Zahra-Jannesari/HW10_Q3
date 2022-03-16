9ffbbf697c85819c714d1525be4205ab49c0f198
Zarisa <jannesari.zarisa@gmail.com>
2022-03-16 13:08:31 +0330

    complete all items in home fragment

--- app/src/main/java/com/zarisa/hw10_q3/DataBank.kt
+++ app/src/main/java/com/zarisa/hw10_q3/DataBank.kt
@@ -1,16 +1,37 @@
 package com.zarisa.hw10_q3
 
 object DataBank {
-    var profilePhotoUrl=R.drawable.profile_photo
-    var userName:String=""
-    var userNationalID:Int=0
-    var userPhone:Int=0
-    var userAddress:String=""
+    var profilePhotoUrl = R.drawable.profile_photo
+    var userName: String = ""
+    var userNationalID: Int = 0
+    var userPhone: Int = 0
+    var userAddress: String = ""
 
 
-    var itemImages= arrayListOf(R.drawable.ali_ghapo)
-    var itemNames= arrayListOf("عمارت عالی قاپو")
-    var itemDescribe= arrayListOf(R.string.Aali_Qapu)
+    var itemImages = arrayListOf(
+        R.drawable.ali_ghapo,
+        R.drawable.lotf_allah,
+        R.drawable.chehel_soton,
+        R.drawable.sadeq_school,
+        R.drawable.monar,
+        R.drawable.naqsh
+    )
+    var itemNames =
+        arrayListOf(
+            "عمارت عالی قاپو",
+            "مسجد شیخ لطف الله",
+            "کاخ چهل ستون",
+            "مدرسه علمیه چهارباغ",
+            "منار جنبان",
+            "میدان نقش جهان"
+        )
+    var itemDescribe = arrayListOf(
+        R.string.Aali_Qapu,
+        R.string.Lotfollah_Mosque,
+        R.string.Chehel_Sotoon,
+        R.string.Sadeq_School,
+        R.string.Naqsh_Jahan
+    )
 //    var userBloodType:BloodType=BloodType.Unknown
 }
 //enum class BloodType{
--- app/src/main/java/com/zarisa/hw10_q3/HomeFragment.kt
+++ app/src/main/java/com/zarisa/hw10_q3/HomeFragment.kt
@@ -31,6 +31,15 @@ class HomeFragment : Fragment() {
     private fun setItems(){
         binding.item1.textView.text=viewModel.getItemName(1)
         viewModel.getItemImage(requireContext(),binding.item1.imageView,1)
+        binding.item2.textView.text=viewModel.getItemName(2)
+        viewModel.getItemImage(requireContext(),binding.item2.imageView,2)
+        binding.item3.textView.text=viewModel.getItemName(3)
+        viewModel.getItemImage(requireContext(),binding.item3.imageView,3)
+        binding.item4.textView.text=viewModel.getItemName(4)
+        viewModel.getItemImage(requireContext(),binding.item4.imageView,4)
+        binding.item5.textView.text=viewModel.getItemName(5)
+        viewModel.getItemImage(requireContext(),binding.item5.imageView,5)
+        binding.item6.textView.text=viewModel.getItemName(6)
+        viewModel.getItemImage(requireContext(),binding.item6.imageView,6)
     }
-
 }
