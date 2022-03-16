32a586d592b864ec9c6c836c575e35f5aac635bb
Zarisa <jannesari.zarisa@gmail.com>
2022-03-16 18:48:08 +0330

    fix setting fragment except change theme

--- app/src/main/java/com/zarisa/hw10_q3/SettingFragment.kt
+++ app/src/main/java/com/zarisa/hw10_q3/SettingFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
+import com.zarisa.hw10_q3.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
-
+    private lateinit var binding:FragmentSettingBinding
+    val viewModelProfile:ViewModelProfile by viewModels()
+    val viewModelItem:ViewModelHomeItems by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -16,10 +21,29 @@ class SettingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View{
         setHasOptionsMenu(false)
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        binding= FragmentSettingBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.index_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerItem.adapter = adapter
+        }
+        setDataInEditTexts()
+    }
+
+    private fun setDataInEditTexts() {
+        binding.name.setText(viewModelProfile.getUserName())
+        binding.id.setText(viewModelProfile.getUserId())
+        binding.phone.setText(viewModelProfile.getUserPhone())
+        binding.address.setText(viewModelProfile.getUserAddress())
     }
 
 }
