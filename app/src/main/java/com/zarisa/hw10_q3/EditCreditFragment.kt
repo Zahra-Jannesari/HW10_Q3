5d2293c2b2a393a509250007e69bd184d1141b98
Zarisa <jannesari.zarisa@gmail.com>
2022-03-29 14:50:04 +0430

    setup binding in edit credit fragment

--- app/src/main/java/com/zarisa/hw10_q3/EditCreditFragment.kt
+++ app/src/main/java/com/zarisa/hw10_q3/EditCreditFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.zarisa.hw10_q3.databinding.FragmentEditCreditBinding
 
 class EditCreditFragment : Fragment() {
-
+    private lateinit var binding:FragmentEditCreditBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,9 +18,9 @@ class EditCreditFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_credit, container, false)
+    ): View {
+        binding = FragmentEditCreditBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
