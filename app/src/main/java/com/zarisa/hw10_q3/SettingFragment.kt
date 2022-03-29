3f06f3b933e5a4c5f9458c4a08fd99dc1dbef4ca
Zarisa <jannesari.zarisa@gmail.com>
2022-03-29 14:28:34 +0430

    set uo access to edit credit fragment from bottom menu in setting fragment

--- app/src/main/java/com/zarisa/hw10_q3/SettingFragment.kt
+++ app/src/main/java/com/zarisa/hw10_q3/SettingFragment.kt
@@ -6,6 +6,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.zarisa.hw10_q3.databinding.FragmentSettingBinding
 
 class SettingFragment : Fragment() {
@@ -36,7 +37,20 @@ class SettingFragment : Fragment() {
 //            binding.spinnerItem.adapter = adapter
 //        }
         setDataInEditTexts()
+        onClicks()
+    }
+
+    private fun onClicks() {
         binding.saveChangeButton.setOnClickListener { saveChanges()}
+        binding.bottomMenu.setOnNavigationItemSelectedListener  { item ->
+            when(item.itemId) {
+               R.id.editCredit->{
+                   findNavController().navigate(R.id.action_nav_setting_to_editCreditFragment)
+                   true
+               }
+                else-> true
+            }
+        }
     }
 
     private fun saveChanges() {
